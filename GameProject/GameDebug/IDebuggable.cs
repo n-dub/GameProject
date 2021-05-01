92db6af0b253a73ace5762185017a14e39725cc7
n-dub <50988318+n-dub@users.noreply.github.com>
2021-05-01 22:33:09 +0500

    Documentation

--- GameProject/GameDebug/IDebuggable.cs
+++ GameProject/GameDebug/IDebuggable.cs
@@ -1,7 +1,15 @@
 ﻿namespace GameProject.GameDebug
 {
+    /// <summary>
+    ///     Interface for debuggable objects
+    /// </summary>
     internal interface IDebuggable
     {
+        /// <summary>
+        ///     Draw debug overlay, e.i. any drawings which can be helpful for debugging
+        ///     of a certain object
+        /// </summary>
+        /// <param name="debugDraw">An instance of <see cref="DebugDraw"/> class that'll be used for drawing</param>
         void DrawDebugOverlay(DebugDraw debugDraw);
     }
 }
